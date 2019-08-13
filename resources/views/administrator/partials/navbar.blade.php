44498b6de1bd15c9bf7512b177a4ef6830244627
Dwi Cahyo <sayabisanyabun@gmail.com>
2019-08-13 18:02:36 +0700

    Fixing 3

--- resources/views/administrator/partials/navbar.blade.php
+++ resources/views/administrator/partials/navbar.blade.php
@@ -20,9 +20,15 @@
                     <i class="far fa-user"></i> Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
-                    <i class="fas fa-sign-out-alt"></i> Logout
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    class="dropdown-item has-icon text-danger">
+                    <i class="fas fa-sign-out-alt"></i> Keluar
                 </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
--- resources/views/partials/nav_star.blade.php
+++ resources/views/partials/nav_star.blade.php
@@ -24,29 +24,13 @@
                     <a class="nav-link" href="property-grid.html">Property</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="blog-grid.html">Blog</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Pages
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="property-single.html">Property Single</a>
-                        <a class="dropdown-item" href="blog-single.html">Blog Single</a>
-                        <a class="dropdown-item" href="agents-grid.html">Agents Grid</a>
-                        <a class="dropdown-item" href="agent-single.html">Agent Single</a>
-                    </div>
+                    <a class="nav-link" href="contact.html">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
             </ul>
         </div>
-        {{-- <button type="button" class="btn btn-b-n navbar-toggle-box-collapse d-none d-md-block"
-            data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-expanded="false">
-            <span class="fa fa-search" aria-hidden="true"></span>
-        </button> --}}
     </div>
 </nav>
 <!--/ Nav End /-->
